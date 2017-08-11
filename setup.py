ea563a98b29ce0863d4f50694b8fb3c78a6d9881
Irmen de Jong <irmen@razorvine.net>
2017-08-11 19:53:41 +0200

    setup

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
+import re
 from setuptools import setup
 
 
 with open("Readme.rst", encoding="utf-8") as f:
     long_description = f.read()
 
+with open("Pyro5/__init__.py") as f:
+    # extract the version string
+    version_line = next(line for line in f if line.startswith("__version__"))
+    pyro_version = re.match("__version__ ?= ?['\"](.+)['\"]", version_line).group(1)
+    if not pyro_version:
+        raise ValueError("cannot determine pyro version")
+    print("Pyro version =", pyro_version)
 
 setup(
     name='Pyro5',
-    version='5.0',
+    version=pyro_version,
     description='Remote object communication library, fifth incarnation',
     long_description=long_description,
     url='https://github.com/irmen/Pyro5',
@@ -36,6 +44,7 @@ setup(
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Topic :: Software Development :: Object Brokering",
         "Topic :: System :: Distributed Computing",
         "Topic :: System :: Networking"
