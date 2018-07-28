1b11eb98106d07d1ba41084e9878a9ba104c2efd
Irmen de Jong <irmen@razorvine.net>
2018-07-28 13:50:32 +0200

    python3.7 deprecation warning fix

--- Pyro5/nameserver.py
+++ Pyro5/nameserver.py
@@ -6,11 +6,15 @@ Pyro - Python Remote Objects.  Copyright by Irmen de Jong (irmen@razorvine.net).
 
 import warnings
 import re
+import sys
 import logging
 import socket
 import time
 import threading
-from collections import MutableMapping
+if sys.version_info <= (3, 4):
+    from collections import MutableMapping
+else:
+    from collections.abc import MutableMapping
 try:
     import sqlite3
 except ImportError:
